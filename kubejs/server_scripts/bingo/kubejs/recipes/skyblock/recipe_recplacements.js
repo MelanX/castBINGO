ec09c53d7dba0d3ed2a43b67651e406eb564391e
MelanX <info@melanx.de>
2021-04-24 22:39:38 +0200

    remove empty schematic to avoid users using schematics

--- kubejs/server_scripts/bingo/kubejs/recipes/skyblock/recipe_recplacements.js
+++ kubejs/server_scripts/bingo/kubejs/recipes/skyblock/recipe_recplacements.js
@@ -31,12 +31,7 @@ onEvent('recipes', event => {
     });
 
     recipes = [
-        shapedRecipe('naturescompass:naturescompass', ['EPE', 'PMP', 'ECE'], {
-            E: '#forge:gems/diamond',
-            P: '#forge:ingots/gold',
-            C: 'minecraft:compass',
-            M: 'minecraft:heart_of_the_sea'
-        })
+
     ];
 
     recipes.forEach(function (recipe) {
